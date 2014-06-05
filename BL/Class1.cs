f431ca7f72a15b2e693494dcf5ee08f2fe5636ad
Joshi <kaffeenutzer@gmail.com>
2014-06-05 16:01:13 +0200

    trr

---
+++ BL/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class Class1
+    {
+
+        private int myVar;
+
+        public int MyProperty
+        {
+            get { return myVar; }
+            set { myVar = value; }
+        }
+        
+    }
+}
